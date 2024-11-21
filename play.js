c12df5e39dc9dbc7e1e5546052d77eab6b9773f7
hajirah84 <mianhajirah@gmail.com>
2024-11-21 02:00:54 -0500

    refactor connect function created client file

--- play.js
+++ play.js
@@ -1,21 +1,21 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
- });
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "localhost",
+//     port: 50541
+//  });
   
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
   
-  conn.on ("data", (event) => {
-    console.log(event)
-  });
-  return conn;
-};
-
+//   conn.on ("data", (event) => {
+//     console.log(event)
+//   });
+//   return conn;
+// };
+const connect = require ('./"client"')
 console.log("Connecting ...");
 connect();
