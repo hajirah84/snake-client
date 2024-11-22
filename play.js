2892c97bfbb0a9d137d3247bd3cf2ba07d7aaa79
hajirah84 <mianhajirah@gmail.com>
2024-11-21 23:46:04 -0500

    refactored setupInput function

--- play.js
+++ play.js
@@ -17,21 +17,24 @@
 //   return conn;
 // };
 const connect = require ('./client')
+const { setupInput } = require("./input");
 console.log("Connecting ...");
 connect();
+setupInput();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
 
-  const handleUserInput = function (key) {
-    if (key === "\u0003") {
-      process.exit();
-    }
-  }
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
 
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+//   const handleUserInput = function (key) {
+//     if (key === "\u0003") {
+//       process.exit();
+//     }
+//   }
+
+//   stdin.on("data", handleUserInput);
+//   return stdin;
+// };
